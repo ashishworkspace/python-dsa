2a6e7b3426ce6a7453ac749488c44004be37ad77
Ashish <ashishizofficial@gmail.com>
2021-05-18 13:40:09 +0530

    modified

--- tree_traverse_breadth_first.py
+++ tree_traverse_breadth_first.py
@@ -10,7 +10,7 @@ class Queue:
     def __init__(self):
         self.queue = []
     def enqueue(self, data):
-        self.queue.insert(0, data)
+        self.queue.append(data)
     def popFromLeft(self):
         tmp = self.queue[0]
         del(self.queue[0])
